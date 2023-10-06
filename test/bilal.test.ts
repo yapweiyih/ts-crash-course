c706ccb30b2d64e66da83f61923e2ae535dd741c
Wei Yih Yap <yihyap@amazon.com>
2023-10-07 07:53:37 +0800

    initial

---
+++ test/bilal.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Bilal from '../lib/bilal-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bilal-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Bilal.BilalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
